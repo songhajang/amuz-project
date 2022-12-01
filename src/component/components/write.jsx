efab03881a1db4f629e8b0f90011baa09ea0a96d
songhajang <angpan2004@gmail.com>
2022-12-01 16:45:50 +0900

    write title 갯수 지정 && 모달창 닫기 오류 수정

--- src/component/components/write.jsx
+++ src/component/components/write.jsx
@@ -3,7 +3,10 @@ import { collection, addDoc, getFirestore } from "firebase/firestore";
 
 function Write({ onClickModal, app, writeModal, getData, setWriteLoading }) {
   const [addData, setAddData] = useState("");
-
+  const onClickWriteModal = () => {
+    onClickModal(false);
+  };
+  // firestore 문서 추가 함수
   const docRef = async (e) => {
     e.preventDefault();
     const db = getFirestore(app);
@@ -30,15 +33,18 @@ function Write({ onClickModal, app, writeModal, getData, setWriteLoading }) {
         cols="30"
         rows="10"
         placeholder="작성할 글을 입력해주세요."
-        value={addData}
-        onChange={(e) => setAddData(e.target.value)}
+        value={addData.substring(0, 19)}
+        onChange={(e) => {
+          setAddData(e.target.value);
+        }}
       ></textarea>
+      <p className="countTitle">{addData.length} / 20</p>
       <div className="popUp-div buttons">
         <input
           type="button"
           value="취소"
           className="cancellation"
-          onClick={onClickModal}
+          onClick={onClickWriteModal}
           style={writeModal ? { display: "block" } : { display: "none" }}
         />
         <input type="submit" value="게시" className="posting" />
